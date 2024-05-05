74fc7ef5afa902b33ee229ea1bc81d0ca3328e17
iamzcr <iamzcr@gmail.com>
2024-05-05 23:19:49 +0800

    add admin vue

--- client/admin/src/main.js
+++ client/admin/src/main.js
@@ -1,14 +1,24 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import router from './router'
-
 const app = createApp(App)
 
-app.use(createPinia())
+//路由
+import router from './router'
 app.use(router)
+//状态甘丽
+import { createPinia } from 'pinia'
+app.use(createPinia())
+
+//注册element-plus
+import ElementPlus from 'element-plus' ;
+import 'element-plus/dist/index.css';
+app.use(ElementPlus);
+
+//注册所有图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.mount('#app')
