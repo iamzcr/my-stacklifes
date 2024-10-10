003bdb1770666ad8226c79e8dbdad9fbac0e949a
zhuchengru <zhuchengru@ijunhai.com>
2024-10-10 20:54:09 +0800

    添加模板

--- server/routers/frontend.go
+++ server/routers/frontend.go
@@ -12,9 +12,8 @@ func FrontendRoutersInit(r *gin.Engine) {
 	commentHandler := frontend.NewCommentHandler()
 	websiteHandler := frontend.NewWebsiteHandler()
 	navHandler := frontend.NewNavHandler()
-	frontendRouter := r.Group("/") //可以加载这个后面
+	frontendRouter := r.Group("/", indexHandler.Index) //可以加载这个后面
 	{
-		frontendRouter.GET("/index", indexHandler.Index)
 		frontendRouter.GET("/article", articleHandler.ArticleList)
 		frontendRouter.GET("/category/:id", articleHandler.CategoryArticleList)
 		frontendRouter.GET("/tag/:id", articleHandler.TagsArticleList)
