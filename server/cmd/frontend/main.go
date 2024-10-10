82c8a406201f904fb007ad6381fd2ad98b6a3e43
zhuchengru <zhuchengru@ijunhai.com>
2024-10-10 14:49:57 +0800

    修改连接配置

--- server/cmd/frontend/main.go
+++ server/cmd/frontend/main.go
@@ -33,7 +33,7 @@ func main() {
 	//路由分组
 	//路由分组抽离成文件，注册路由
 	routers.FrontendRoutersInit(r)
-	fmt.Println("app_config:", err)
+	fmt.Println("app_config:", conf.AppConfig)
 	//启动web服务
 	err = r.Run(conf.AppConfig.Common.FrontendListenPort)
 	if err != nil {
