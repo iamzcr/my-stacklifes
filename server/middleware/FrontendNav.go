463c2806ca5b3e8693ce6b5f7b1f80d5cec525dc
zhuchengru <zhuchengru@ijunhai.com>
2024-10-15 19:28:02 +0800

    rea

--- server/middleware/FrontendNav.go
+++ server/middleware/FrontendNav.go
@@ -1,12 +1,14 @@
 package middleware
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func GetFrontendNav() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Set("nav", "test")
+		fmt.Println("nav set in middleware:", c.MustGet("nav"))
 		c.Next()
 	}
 }
