0a9086d48cdc07feb560a4ebe6659750d59fb54e
zhuchengru <zhuchengru@ijunhai.com>
2024-10-16 18:05:48 +0800

    bug

--- server/routers/frontend.go
+++ server/routers/frontend.go
@@ -19,16 +19,11 @@ func FrontendRoutersInit(r *gin.Engine) {
 	indexHandler := frontend.NewIndexHandler()
 	messageHandler := frontend.NewMessageHandler()
 	commentHandler := frontend.NewCommentHandler()
-	websiteHandler := frontend.NewWebsiteHandler()
-	navHandler := frontend.NewNavHandler()
-
 	r.GET("/", indexHandler.Index)
 	r.GET("/article", articleHandler.ArticleList)
 	r.GET("/category/:id", articleHandler.CategoryArticleList)
-	r.GET("/tag/:id", articleHandler.TagsArticleList)
+	r.GET("/tag/id/:id", articleHandler.TagsArticleList)
 	r.GET("/article/detail/:id", articleHandler.ArticleDetail)
-	r.GET("/nav", navHandler.GetNavList)
-	r.GET("/website", websiteHandler.WebsiteFrontendList)
 	r.POST("/message", messageHandler.Update)
 	r.POST("/comment", commentHandler.Update)
 }
