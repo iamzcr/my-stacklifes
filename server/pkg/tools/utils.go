b49734881c7a9c6f075a225f8e69636cfc7ca3f8
zhuchengru <zhuchengru@ijunhai.com>
2024-10-17 09:52:09 +0800

    bug

--- server/pkg/tools/utils.go
+++ server/pkg/tools/utils.go
@@ -72,7 +72,7 @@ func ConvertMarkdownToHTML(markdownContent []byte) (string, error) {
 	return buf.String(), nil
 }
 
-func UnixToTime(timestamp int) string {
+func UnixToTime(timestamp int64) string {
 	t := time.Unix(int64(timestamp), 0)
 	return t.Format("2006-01-02 15:04:05")
 }
