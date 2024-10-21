55e583992a97f3e117851494908e0d50e0be4246
zhuchengru <zhuchengru@ijunhai.com>
2024-10-21 17:50:11 +0800

    test

--- server/models/common.go
+++ server/models/common.go
@@ -6,13 +6,19 @@ type CommonField struct {
 }
 
 type PageInfo struct {
-	Start  int   `form:"start" json:"start"`
-	Length int   `form:"length" json:"length"`
-	Total  int64 `form:"total" json:"total"`
+	Page     int   `form:"page" json:"page"`
+	PageSize int   `form:"page_size" json:"page_size"`
+	Total    int64 `form:"total" json:"total"`
 }
 
 func (p *PageInfo) GetPageInfo() (limit, offset int) {
-	limit = p.Length
-	offset = p.Start
+	if p.Page < 1 {
+		p.Page = 1
+	}
+	limit = p.PageSize
+	if p.PageSize < 1 {
+		limit = 5
+	}
+	offset = (p.Page - 1) * limit
 	return
 }
