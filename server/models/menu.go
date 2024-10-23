514aaf371cee93adcbe420fd7cd53550dda1552a
zhuchengru <zhuchengru@ijunhai.com>
2024-10-23 14:30:15 +0800

    test

--- server/models/menu.go
+++ server/models/menu.go
@@ -1,16 +1,16 @@
 package models
 
 type Menu struct {
-	Id     int    `json:"id"   gorm:"column:id;type:int(11) signed;primary_key;AUTO_INCREMENT" `
-	Name   string `json:"name" form:"name"  binding:"required"`
-	Url    string `json:"url"  form:"url" binding:"required"`
-	Parent int    `json:"parent"  form:"parent,default=0" binding:"required"`
-	Weight int    `json:"weight" form:"weight,default=0" binding:"required"`
-	Type   int    `json:"type" form:"type,default=1"`
-	Status int    `json:"status" gorm:"column:status;type:int(11);default:1;" form:"status,default=1"`
-	Author string `json:"author" form:"author,default=nicholas"`
-	Icon   string `json:"icon" form:"icon"`
-	Mark   string `json:"mark" form:"mark" `
+	Id     int    `json:"id" gorm:"column:id;type:int(11);primary_key;AUTO_INCREMENT" `
+	Name   string `json:"name" `
+	Url    string `json:"url"  `
+	Parent int    `json:"parent" gorm:"column:parent;type:int(11);default:0"`
+	Weight int    `json:"weight" gorm:"column:weight;type:int(11);default:0"`
+	Type   int    `json:"type" gorm:"column:type;type:int(11);default:1"`
+	Status int    `json:"status" gorm:"column:status;type:int(11);default:1" `
+	Author string `json:"author" gorm:"column:author;type:string;default:nicholas"`
+	Icon   string `json:"icon"`
+	Mark   string `json:"mark" `
 	CommonField
 }
 
