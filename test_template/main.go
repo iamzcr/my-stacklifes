40d57d9783c08eda8303c0cd46378bafd77aa52b
zhuchengru <zhuchengru@ijunhai.com>
2024-11-05 18:12:55 +0800

    模板

--- test_template/main.go
+++ test_template/main.go
@@ -7,41 +7,27 @@ import (
 	"strings"
 )
 
-func visit(rootPath string) filepath.WalkFunc {
-	return func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		// Skip printing the .idea directory
-		if info.IsDir() && info.Name() == ".idea" {
-			return filepath.SkipDir
-		}
-
-		// Get the relative path
-		relPath, err := filepath.Rel(rootPath, path)
-		if err != nil {
-			return err
-		}
-
-		if relPath == "." {
-			return nil
-		}
-
-		depth := strings.Count(relPath, string(filepath.Separator)) - 1
-		indent := strings.Repeat("  ", depth)
-		if depth > 0 {
-			indent += "---"
-		}
-		fmt.Printf("%s %s\n", indent, relPath)
-		return nil
+func visit(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
 	}
+
+	// Skip printing the .idea directory
+	if info.IsDir() && (info.Name() == "." || info.Name() == ".idea") {
+		return filepath.SkipDir
+	}
+	depth := strings.Count(path, string(os.PathSeparator)) - strings.Count(".", string(os.PathSeparator))
+	indent := strings.Repeat("  ", depth)
+	if depth > 0 {
+		indent += "--"
+	}
+	fmt.Printf("%s %s\n", indent, path)
+	return nil
 }
 
 func main() {
-	dir := filepath.Dir(os.Args[0]) // Get the directory of the running executable
-	root := filepath.Join(dir, "test_template")
-	err := filepath.Walk(root, visit(root))
+	root := "."
+	err := filepath.Walk(root, visit)
 	if err != nil {
 		fmt.Println(err)
 	}
